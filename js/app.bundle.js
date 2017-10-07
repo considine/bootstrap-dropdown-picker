924834e55335544e7af0f6a8d99a8945df037c31
John P Considine <jconsidi@nd.edu>
2017-10-07 04:49:56 -0500

    feat: allow multiple dropdowns on same page w/o collisions

--- js/app.bundle.js
+++ js/app.bundle.js
@@ -10354,21 +10354,28 @@ module.exports = {
    */
   listenForDropDown : function (options) {
     options = defaultFor(options, {});
-    var dropdownMenuSelector = defaultFor(options.menuSelector, ".dropdown-module .dropdown-menu a");
-    var labelTargetSelector = defaultFor(options.labelTargetSelector, ".dropdown-module .dropdown-selection-wrapper .dropdown-selection");
-    var inputTargetSelector = defaultFor(options.inputTargetSelector, ".dropdown-module .dropdown-selection-wrapper input");
+    var rootModuleSelector = defaultFor(options.rootModuleSelector, '.dropdown-module');
+    var dropdownMenuSelector = defaultFor(options.menuSelector, " .dropdown-menu a");
+    var labelTargetSelector = defaultFor(options.labelTargetSelector, ".dropdown-selection-wrapper .dropdown-selection");
+    var inputTargetSelector = defaultFor(options.inputTargetSelector, ".dropdown-selection-wrapper input");
     var inputTargetName = defaultFor(options.inputTargetName, "input-dropdown");
 
     console.log(labelTargetSelector);
     // var labelTargetSelector = options.
     var dropdownDataSelector = options.dataselector || "dropdownval";
 
-    $(dropdownMenuSelector).click(function() {
+    $(rootModuleSelector + " " + dropdownMenuSelector).click(function() {
       var text = $( this ).text();
-      console.log(text);
-      $(labelTargetSelector).text(text);
-      $(inputTargetSelector).attr('name', inputTargetName)
-      $(inputTargetSelector).val( $( this ).data(dropdownDataSelector) );
+      // $(this).parentsUntil( rootModuleSelector ).remove();
+
+
+
+      $( this ).parentsUntil( rootModuleSelector ).parent().find(labelTargetSelector).text(text);
+      $( this ).parentsUntil( rootModuleSelector ).parent().find(inputTargetSelector).attr('name', inputTargetName)
+      $( this ).parentsUntil( rootModuleSelector ).parent().find(inputTargetSelector).val( $( this ).data(dropdownDataSelector) );
+      // $(labelTargetSelector).text(text);
+      // $(inputTargetSelector).attr('name', inputTargetName)
+      // $(inputTargetSelector).val( $( this ).data(dropdownDataSelector) );
       // $(inputTargetSelector).val( $( this ).data( dropdownDataSelector) );
     });
   }
